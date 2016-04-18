1964d454acdff312d7009e58671ce4b483ced8bc
Roman Finkelnburg <roman.finkelnburg@olx.com>
2016-04-18 11:12:52 +0200

    - added webservice cookbook example

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,10 +2,11 @@
 
 # Do Chef provisioning
 cd $CHEF_HOME
-chef-client --local-mode hello.rb
+#chef-client --local-mode hello.rb
 #chef-client --local-mode goodbye.rb
 #chef-client --local-mode aptupdate.rb
 #chef-client --local-mode webserver.rb
+chef-client --local-mode --runlist 'recipe[learn_chef_apache2]'
 
 # Do some nice stuff (e.g. getting a shell ;)
 /bin/bash
